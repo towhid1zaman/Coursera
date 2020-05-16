33da25b35495222afa0773d9519ec1bce5459990
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-05-16 16:41:34 +0600

    week2

---
+++ Algorithms-On-Graphs-Coursera/week2_decomposition2/1_acyclicity/acyclicity.cpp
@@ -0,0 +1,43 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+
+int dfs(const vector<vector<int> > &adj, int x, vector<int> &visited, vector<int> &stack) {
+  visited[x] = 1;
+  stack[x] = 1;
+  for (vector<int>::const_iterator it = adj[x].begin(); it != adj[x].end(); ++it) {
+    if (!visited[*it] && dfs(adj, *it, visited, stack))
+	  return 1;
+	else if(stack[*it])
+	  return 1;
+  }
+  stack[x] = 0;
+  return 0;
+}
+
+int acyclic(vector<vector<int> > &adj) {
+  vector<int> visited(adj.size());
+  vector<int> stack(adj.size());
+  for (int i = 0; i < adj.size(); i++)
+  {
+    if (!visited[i]) {
+      if (dfs(adj, i, visited, stack))
+        return 1;
+    }
+  }
+  return 0;
+}
+
+int main() {
+  size_t n, m;
+  std::cin >> n >> m;
+  vector<vector<int> > adj(n, vector<int>());
+  for (size_t i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    adj[x - 1].push_back(y - 1);
+  }
+  std::cout << acyclic(adj);
+}
